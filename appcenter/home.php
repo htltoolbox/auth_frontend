f0e28f3f3a4fbdc0cb49e6570b93c81c91569d75
Philipp Lackinger <Phillackinger@gmx.at>
2021-04-29 20:38:13 +0200

    ✨ Updated Homescreen
    + Added new App-Weberboard

--- appcenter/home.php
+++ appcenter/home.php
@@ -11,13 +11,26 @@ if (empty($userdata = json_decode($_SESSION['userdata']))) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
+
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+
+    <link rel="apple-touch-icon" href="icon.png">
+
+
     <title>HTL-Toolbox | Apps</title>
 
     <link rel="stylesheet" href="../css/util.css"
     <link rel="stylesheet" href="../css/w3.css">
+
+    <link rel="stylesheet" href="../css/content.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" href="../css/main.css">
+
+
     <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 
     <style>
@@ -35,6 +48,18 @@ if (empty($userdata = json_decode($_SESSION['userdata']))) {
             background-size: 100%;
         }
 
+        .grid-item:hover,
+        .grid-item:focus {
+            box-shadow: 0 0.5em 0.5em -0.4em #1f1f1f;
+            transform: translateY(-0.25em);
+        }
+
+        .grid-item:hover, .grid-item:active {
+            animation: box-shadow;
+            animation-duration: 250ms;
+            animation-timing-function: ease-in-out;
+        }
+
         .app-icon {
             width: 100px;
             height: 100px;
@@ -46,23 +71,32 @@ if (empty($userdata = json_decode($_SESSION['userdata']))) {
 </head>
 <body>
 <div class="main">
-    <div class="navbar"><?php include $_SERVER["DOCUMENT_ROOT"] . '/allSiteElements/navbar/menu.php'; ?></div>
-    <div class="head">
+    <div id="topbar">
+        <div class="al_start">
+            <div id="burger" class="menuitem"><img src="../content/Icons/burger.svg" class="icon" alt="burger"></div>
+            <div id="search" class="menuitem"><img src="../content/Icons/search.svg" class="icon" alt="search"><input
+                        alt="serach"></div>
+        </div>
+        <img src="../content/Logo.png" class="toplogo" alt="Logo">
+        <div id="user" class="menuitem"><img src="../content/Icons/user-solid.svg" class="icon" alt="user"><span
+                    style="margin-top: 10px"><?php echo $userdata->NAME . " " . $userdata->LASTNAME ?></span></div>
+    </div>
+    <div class="head text-center">
         <h1>Hallo <?php echo $userdata->NAME . " " . $userdata->LASTNAME ?></h1>
     </div>
     <div class="grid-container">
         <div class="grid-item flex-c-b" style="background-image: url('Icons/LT_Logo965x965.png')">
-            <h3 class="app-name">Leiterplattentechnik</h3>
+            <h3 class="app-name text-white p-b-15">Leiterplattentechnik</h3>
         </div>
         <div class="grid-item flex-c-b" style="background-image: url('Icons/htltoolbox (2).png')">
             <h3 class="app-name"></h3>
         </div>
-        <div class="grid-item flex-c-b" style="background-image: url('Icons/LT_Logo965x965.png')">
-            <h3 class="app-name">Leiterplattentechnik</h3>
-        </div>
+        <a href="https://whiteboard.philsoft.at/">
+            <div class="grid-item flex-c-b" style="background-image: url('Icons/weberboard.png')">
+                <h3 class="app-name text-white p-b-15">Weberboard</h3>
+            </div>
+        </a>
     </div>
 </div>
-
-
 </body>
 </html>
